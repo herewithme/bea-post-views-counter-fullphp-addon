a3070d6015afbbd3d0d83ec24d8cbcb2a58ea031
Amaury Balmer <amaury@beapi.fr>
2013-07-04 16:08:07 +0200

    first commit for addon

---
+++ bea-post-views-counter-fullphp-addon.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ Plugin Name: BEA Post Views Counter - FullPHP Addon
+ Version: 0.1
+ Plugin URI: https://github.com/herewithme/bea-post-views-counter
+ Description: This addon to BEA Post Views Counter add a full PHP implementation for increment counter.
+ Author: Amaury Balmer
+ Author URI: http://www.beapi.fr
+ Domain Path: languages
+ Network: false
+ Text Domain: bea-post-views-counter-fullphp-addon
+ Depends: bea-post-views-counter
+
+ ----
+
+ Copyright 2013 Amaury Balmer (amaury@beapi.fr)
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+// don't load directly
+if ( !defined('ABSPATH') )
+	die('-1');
+
+// Plugin URL and PATH
+define('BEA_PVC_FPA_URL', plugin_dir_url ( __FILE__ ));
+define('BEA_PVC_FPA_DIR', plugin_dir_path( __FILE__ ));
+
+add_action('plugins_loaded', create_function('', 'return new BEA_PVC_FPA_Main();'), 11 );
---
+++ classes/main.php
@@ -0,0 +1,34 @@
+<?php
+class BEA_PVC_FPA_Main {
+	/**
+	 * Register hooks
+	 */
+	public function __construct() {
+		if ( !defined('BEA_PVC_VERSION') ) {
+			return false;
+		}
+
+		add_action('bea_pvc_counter_url', array(__CLASS__, 'bea_pvc_counter_url'), 10, 2 );
+		add_action('bea_pvc_get_settings_fields', array(__CLASS__, 'bea_pvc_get_settings_fields'), 10, 1 );
+
+		return true;
+	}
+	
+
+	public static function bea_pvc_counter_url( $url = '', $current_options = array() ) {
+		global $wpdb;
+
+		if ( isset($current_options['mode']) && $current_options['mode'] == 'js-php' ) { // Pure PHP
+			$url = BEA_PVC_FPA_URL . 'tools/counter.php?post_id='.  get_queried_object_id().'&blog_id='.$wpdb->blogid;
+		}
+
+		return $url;
+	}
+	
+	public static function bea_pvc_get_settings_fields( $settings_fields ) {
+		$settings_fields['bea-pvc-main'][0]['options']['js-php'] = __('JS call with pure PHP script (best performance)', 'bea-post-views-counter');
+		$settings_fields['bea-pvc-main'][0]['desc'] = __('Mode <strong>inline</strong> is the simplest, most reliable, but it is not compatible with plugins static cache.<br />The two modes "JS Call" add asynchronous JavaScript code in the footer of your site for compatibilizing the number of views. The difference between <strong>WordPress</strong> and <strong>PHP Pure</strong> is the mechanism used to update the counters in the database. The <strong>pure PHP</strong> mode is on average 10 times more efficient than the WP mode because it does not load WordPress!<br />However, the <strong>pure PHP</strong> mode sometimes have problems operating in some accommodation, this is the reason why this is not the default mode.', 'bea-post-views-counter');
+		
+		return $settings_fields;
+	}
+}
